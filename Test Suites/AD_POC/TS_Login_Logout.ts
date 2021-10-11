53674149b9e959d9a11b7d9ba7cdfd7879bbb0bf
Riza <Riza@DESKTOP-FQ1P4P9>
2021-10-11 14:16:15 +0700

    added TC Login & TC Logout v.1.0

---
+++ Test Suites/AD_POC/TS_Login_Logout.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_Login_Logout</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>26387340-b15a-417c-8dc3-b86154a54ce3</testSuiteGuid>
+   <testCaseLink>
+      <guid>4e889918-c73b-4e17-9318-36ce818d8609</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/AD_Login/TC_Login</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b0e4b8e8-1e7c-4878-8725-8b1b606eabb5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/AD_Logout/TC_Logout</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
