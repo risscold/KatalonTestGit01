f7120e1208c1df7711071791230147a89f92ce68
Riza <Riza@DESKTOP-FQ1P4P9>
2021-10-11 10:45:18 +0700

    added

---
+++ Scripts/ByRecord/Login/Script1633923849800.groovy
@@ -0,0 +1,29 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+Mobile.startApplication('C:\\Users\\Apps\\Android\\Alodokter_user_v3.6.0_staging_debug_300921_1624.apk', false)
+
+Mobile.tap(findTestObject('ByRecord/Login/android.widget.TextView - Masuk menggunakan Email'), 0)
+
+Mobile.setText(findTestObject('ByRecord/Login/android.widget.EditText - Email'), 'autouser3thsize@mail.com', 0)
+
+Mobile.setText(findTestObject('ByRecord/Login/android.widget.EditText - Kata Sandi'), '123456', 0)
+
+Mobile.tap(findTestObject('ByRecord/Login/android.widget.TextView - Masuk'), 0)
+
